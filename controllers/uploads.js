d98a7bbd42d834bca56b1e3ba0fccfd9ddfcd394
Michael Rodríguez <60236402+xMichaelRodriguez@users.noreply.github.com>
2021-04-20 17:26:26 -0600

    mejorando

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,3 +1,8 @@
+const path = require("path");
+const fs = require("fs");
+
+const pathResolve = path.resolve(process.cwd(), "./uploads");
+
 const upload = async (req, res) => {
   const path = req.files;
 
@@ -9,7 +14,7 @@ const upload = async (req, res) => {
       });
     }
   } catch (err) {
-    console.log(err);
+    console.log("Error: ", err);
     return res.status(400).json({
       ok: false,
       msg: "something went wrong",
@@ -17,6 +22,72 @@ const upload = async (req, res) => {
   }
 };
 
+const searchFile = (oldName, newName) => {
+  const folder = oldName.split("/")[0];
+  const originalOldName = oldName.split("/")[1];
+  console.log(folder);
+
+  if (oldName.indexOf("/") >= -1) {
+    fs.readdirSync(`${pathResolve}/${folder}`).forEach((img) => {
+      if (img === originalOldName) {
+        fs.rename(
+          `${path.join(pathResolve, folder)}/${originalOldName}`,
+          `${path.join(pathResolve, folder)}/${newName}${path.extname(
+            oldName
+          )}`,
+          (err) => {
+            console.log("Image renamed");
+            console.log(err);
+          }
+        );
+      }
+    });
+  }
+
+  fs.readdirSync(pathResolve).forEach((file) => {
+    if (file === oldName) {
+      console.log(oldName);
+      fs.rename(
+        path.join(pathResolve, oldName),
+        path.join(pathResolve, `${newName}${path.extname(oldName)}`),
+        () => {
+          console.log("ok");
+          return "Image Renamed";
+        }
+      );
+    }
+  });
+};
+
+const renameFile = async (req, res) => {
+  const { newName, oldName } = req.body;
+
+  try {
+    if (newName && oldName) {
+      const resp = await searchFile(oldName, newName);
+
+      if (resp === "Image Renamed") {
+        return res.satus(200).json({
+          ok: true,
+          msg: resp,
+        });
+      } else {
+        return res.status(400).json({
+          ok: false,
+          msg: resp,
+        });
+      }
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      ok: false,
+      msg: err,
+    });
+  }
+};
+
 module.exports = {
   upload,
+  renameFile,
 };
--- routes/uploadsRute.js
+++ routes/uploadsRute.js
@@ -4,11 +4,22 @@
 
 const { Router } = require("express");
 const multer = require("multer");
-const { upload } = require("../controllers/uploads");
+const { check } = require("express-validator");
+const { upload, renameFile } = require("../controllers/uploads");
+const { validarCampos } = require("../middleware/validationFields");
 const { uploadImage } = require("../middleware/storage");
 //init routes
 const rutes = Router();
 
 rutes.post("/", uploadImage.array("image", 5), upload);
+rutes.put(
+  "/rename",
+  [
+    check("oldName", "The old name of file is required").notEmpty(),
+    check("newName", "The new name of file is required").notEmpty(),
+    validarCampos,
+  ],
+  renameFile
+);
 
 exports.uploadRoute = rutes;
